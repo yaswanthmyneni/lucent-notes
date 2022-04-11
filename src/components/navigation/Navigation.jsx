0d0314bb0616f1fcb371df8000a4a8be00b4ce45
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-04-11 12:42:07 +0530

    feat: added navigation component

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,48 @@
+import "./navigation.css";
+import { NavLink, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const Navigation = () => {
+  const encodedToken = localStorage.getItem("token");
+  const [logout, setLogout] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (logout) {
+      window.location.reload();
+    }
+  }, [logout]);
+
+  return (
+    <header className="header">
+      <div className="navbar-container">
+        <NavLink className="navbar" to="/">
+          <h1 className="navbar-m-lr-1">LUCENT NOTES</h1>
+        </NavLink>
+        {!encodedToken && (
+          <NavLink
+            className="navbar navbar-m-left-auto"
+            to="/signin"
+            state={{ from: location }}
+          >
+            <h5 className="navbar-m-lr-1">LOGIN</h5>
+          </NavLink>
+        )}
+        {encodedToken && (
+          <NavLink
+            className="navbar navbar-m-left-auto"
+            to="/logout"
+            onClick={() => {
+              setLogout(true);
+              localStorage.clear();
+            }}
+          >
+            <h5 className="navbar-m-lr-1">LOGOUT</h5>
+          </NavLink>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export { Navigation };
