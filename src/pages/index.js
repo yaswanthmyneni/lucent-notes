fbcfa9c1559d678a73c723f4eab49afd13e2a52b
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-05-21 13:20:41 +0530

    feat: added page not found route

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ export { LandingPage } from "./landing-page/LandingPage";
 export { NotesPage } from "./notes-page/NotesPage";
 export { TrashPage } from "./trash-page/TrashPage";
 export { ArchivePage } from "./archive-page/ArchivePage";
+export { PageNotFound } from "./page-not-found/PageNotFound";
 export { SignInPage } from "./authentication-pages/signin-page/SignInPage";
 export { LogoutPage } from "./authentication-pages/logout-page/LogoutPage";
 export { SignUpPage } from "./authentication-pages/signup-page/SignUpPage";
---
+++ src/pages/page-not-found/PageNotFound.jsx
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+  return (
+    <main className="text-center">
+      <h1>ERROR 404: Page Not Found!</h1>
+    </main>
+  );
+};
+
+export { PageNotFound };
